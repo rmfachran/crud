4eef46182dab5403a3e87bbb730dea7b2cb46ac1
rmfachran <61181145+rmfachran@users.noreply.github.com>
2023-06-07 13:56:11 +0700

    Delete dto directory

--- dto/response-meta.go
+++
@@ -1,8 +0,0 @@
-package dto
-
-type ResponseMeta struct {
-	Success      bool   `json:"success"`
-	MessageTitle string `json:"messageTitle"`
-	Message      string `json:"message"`
-	ResponseTime string `json:"responseTime"`
-}
